b90267c60c2e0f65bfdd667afc07bedd69d27d2b
Antonios Pavlakis <antonios.pavlakis@staysafeapp.com>
2020-06-22 14:26:30 +0100

    Initialise project for API

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
