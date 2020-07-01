76b4343b7f110b5046fda1016dc81769e869398e
Antonios Pavlakis <antonios.pavlakis@staysafeapp.com>
2020-07-01 22:33:53 +0100

    Add strict types on all files

--- tests/phpunit/Entity/EventTest.php
+++ tests/phpunit/Entity/EventTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace PHPMinds\Unit\Entity;
 
 use App\Entity\Event;
--- tests/phpunit/Transformer/EventTransformerTest.php
+++ tests/phpunit/Transformer/EventTransformerTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace PHPMinds\Unit\Transformer;
 
 use App\Entity\Event;
